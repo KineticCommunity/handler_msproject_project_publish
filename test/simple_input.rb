c1f6c90d6b0c2be36c0846c9b087750811a5ac6a
Scott Gerike <sgerike@scott-macbook.local>
2015-09-29 12:45:21 -0500

    Initial Commit

---
+++ test/simple_input.rb
@@ -0,0 +1,11 @@
+{
+  'info' => {
+    'username' => '',
+    'password' => '',
+    'ms_project_location' => '',
+    'enable_debug_logging' => 'Yes'
+  },
+  'parameters' => {
+    'project_id' => '0e993ca3-4a14-e511-80c5-00155d3cea0d'
+  }
+}
